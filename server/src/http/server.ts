2a1c8ff470f82f55e5c70a255d42a6bf30409f13
Key Yu Wan <keyflcbyuwan@gmail.com>
2024-09-10 14:53:01 -0300

    feat: get week pending goals

--- server/src/http/server.ts
+++ server/src/http/server.ts
@@ -6,11 +6,18 @@ import {
   type ZodTypeProvider,
 } from 'fastify-type-provider-zod'
 import { createGoal } from '../use-cases/create-goal'
+import { getWeekPendingGoals } from '../use-cases/get-week-pending-goals'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.setValidatorCompiler(validatorCompiler)
 app.setSerializerCompiler(serializerCompiler)
+
+app.get('/pending-goals', async () => {
+  const { pendingGoals } = await getWeekPendingGoals()
+
+  return { pendingGoals }
+})
 
 app.post(
   '/goals',
